49a314f06d874f80256ffda4fe280a2cd2e29894
oikade-co-uk <61059965+oikade-co-uk@users.noreply.github.com>
2022-07-21 14:52:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit;
 
 streamlit.title("Casimero's wonderful diner");
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('Lunch Menu')
+streamlit.text(' Something not entirely unlike meatballs')
